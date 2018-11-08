84b6fb56b26f470e9b6adcfe80b63f936857ee79
Cameron Devine <camerondtrain@gmail.com>
2018-11-07 22:25:15 -0800

    hid the search button for now

--- assets/allergies.js
+++ assets/allergies.js
@@ -87,12 +87,11 @@ function startApp(data) {
 						<span class="mdl-layout-title">{{name}}'s Allergies</span>
 						<div class="mdl-layout-spacer"></div>
 						<div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-							<label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+							<label style="display:none" class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
 								<i class="material-icons">search</i>
 							</label>
-							<div class="mdl-textfield__expandable-holder">
-								<input class="mdl-textfield__input" type="text" name="sample"
-							id="fixed-header-drawer-exp">
+							<div styte="display:none" class="mdl-textfield__expandable-holder">
+								<input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
 							</div>
 						</div>
 					</div>
